daacad11b3916ae966dbcb83261a51c571297ace
Jason Sanford <jasonsanford@gmail.com>
2014-08-13 10:38:29 -0400

    Add usermarker

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiamNzYW5mb3JkIiwiYSI6InRJMHZPZFUifQ.F4DMGoNgU3r2AWLY0Eni-w';
 
 var map = new L.mapbox.Map('map-container', 'jcsanford.j25ef8lg');
+
+var loc = [40, -85];
+
+var marker = L.userMarker(loc, {pulsing: true, accuracy: 400, smallIcon: true});
+map.setView(loc, 10);
+marker.addTo(map);
